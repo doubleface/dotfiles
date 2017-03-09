ff7cf94ff64082fcc58c4f84c943e661b5b5bd94
doubleface <christophe@thiriot.xyz>
2017-03-09 17:57:40 +0100

    Also copy .hgrc

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ ln -vfs $ROOTPATH/.bash_perso
 ln -vfs $ROOTPATH/.bashrc
 ln -vfs $ROOTPATH/.gitconfig
 ln -vfs $ROOTPATH/.gitignore
+ln -vfs $ROOTPATH/.hgrc
 ln -vfs $ROOTPATH/.inputrc
 ln -vfs $ROOTPATH/.tmux.conf
 ln -vfs $ROOTPATH/.vimrc
